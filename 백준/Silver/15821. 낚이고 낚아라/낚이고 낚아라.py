a86428f669f2183a43dc4cc4273f6dfd4730602d
coouir <166575866+coouir@users.noreply.github.com>
2024-08-04 00:42:09 +0900

    [Silver III] Title: 낚이고 낚아라, Time: 36 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/15821. 낚이고 낚아라/낚이고 낚아라.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+l = []
+for i in range(N):
+    Pi = int(input())
+    arr = list(map(int, input().split()))
+    m = 0
+    for j in range(len(arr)):
+        if j%2 == 0:
+            m = max(m, arr[j]**2 + arr[j+1]**2)
+    l.append(m)
+l.sort()
+print("{:.2f}".format(l[K-1]))

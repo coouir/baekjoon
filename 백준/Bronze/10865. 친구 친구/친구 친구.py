88f3b8f044e6cf4cecde89c9247e8109ba9781ee
coouir <166575866+coouir@users.noreply.github.com>
2024-08-18 15:39:40 +0900

    [Bronze III] Title: 친구 친구, Time: 748 ms, Memory: 31832 KB -BaekjoonHub

---
+++ 백준/Bronze/10865. 친구 친구/친구 친구.py
@@ -0,0 +1,11 @@
+input = __import__('sys').stdin.readline
+
+N, M = map(int, input().split())
+
+cnt = [0] * (N+1)
+for i in range(M):
+    A, B = map(int, input().split())
+    cnt[A] += 1
+    cnt[B] += 1
+for i in range(1, N+1):
+    print(cnt[i])

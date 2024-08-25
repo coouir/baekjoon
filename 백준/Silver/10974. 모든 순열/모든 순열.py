3ba47b90432b8884a6868d804a460a2350434f48
coouir <166575866+coouir@users.noreply.github.com>
2024-08-25 12:26:26 +0900

    [Silver III] Title: 모든 순열, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,11 +1,8 @@
 from itertools import permutations as pm
 
 N = int(input())
+arr = [i for i in range(1, N+1)]
 
-l = []
-for _ in range(1, N+1):
-    l.append(_)
-
-for t in pm(l, N):
+for t in pm(arr, N):
     print(' '.join(map(str, t)))
 

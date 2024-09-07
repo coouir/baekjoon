fc72a4287c04476ac436c99a0aa2a256a13ea5eb
coouir <166575866+coouir@users.noreply.github.com>
2024-09-07 16:04:05 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7428 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,10 @@
+input = __import__('sys').stdin.readline
+
+N = int(input())
+arr = [0]*10001
+for i in range(N):
+    arr[int(input())] += 1
+
+for i in range(10001):
+    for _ in range(arr[i]):
+        print(i)

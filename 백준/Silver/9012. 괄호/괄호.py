951bd1cd7a8cbd9c21adb8ec781bd4ed193477fe
coouir <166575866+coouir@users.noreply.github.com>
2024-10-19 15:20:20 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,27 +1,24 @@
 # function
-def function(s):
+def f(s):
     if len(s) == 0:
-        return
+        return True
      
-    fl = len(s)
+    fl = len(s) # 변경 전 s의 길이
     for i in range(len(s)-1):
         if s[i] + s[i+1] == '()':
             s.pop(i); s.pop(i)
             break
-    if fl == len(s):
-        return
+    if fl == len(s): # 변경 전 s와 변경 후 s의 길이 비교
+        return False
 
-    return function(s)
+    return f(s)
 
 # main
 T = int(input())
 
 for i in range(T):
-    string = input()
-    sList = list(string)
-
-    function(sList)
-    if len(sList) == 0:
+    s = list(input())
+    if f(s):
         print('YES')
     else:
         print('NO')

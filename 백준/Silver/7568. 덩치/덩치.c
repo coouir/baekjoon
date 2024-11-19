3a4918610008acbc00ca63c4eee37abe756c9875
coouir <166575866+coouir@users.noreply.github.com>
2024-11-19 23:45:33 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef struct {
+    int weight;
+    int height;
+    int rank;
+} P;
+
+int main() {
+    P p[50];
+    
+    int N, i, j;
+    scanf("%d", &N);
+    for (i=0; i<N; i++) {
+        scanf("%d %d", &p[i].weight, &p[i].height);
+    }
+
+    for (i=0; i<N; i++) {
+        int cnt = 1;
+        for (j=0; j<N; j++) {
+            if (i==j) {
+                continue;
+            }
+            if (p[j].weight > p[i].weight && p[j].height > p[i].height)
+                cnt++;
+        }
+        printf("%d ", cnt);
+    }
+    return 0;
+}

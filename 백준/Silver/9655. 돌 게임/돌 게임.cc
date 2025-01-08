f2ac7f62141c2a2ebcbb77176963752cb0731257
coouir <166575866+coouir@users.noreply.github.com>
2025-01-08 15:14:46 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    cout << ((N%2 == 1) ? "SK":"CY");
+    return 0;
+}
+
+// N = 1, 1 상근
+// N = 2, 1 2 창영
+// N = 3, 1 2 3 상근
+// N = 4, 1 2 3 4 창영
+// N = 5, 1 2 3 4 5 상근
+// N = 6, 1 2 3 4 5 6 창영

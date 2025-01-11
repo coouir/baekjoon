99224eef5c6fa6c521bb274328f0b2ea5a10df37
coouir <166575866+coouir@users.noreply.github.com>
2025-01-12 00:48:55 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    while (true) {
+        cin >> A >> B;
+        if (cin.eof()) break;
+        else cout << A+B << endl;
+    }
+    return 0;
+}

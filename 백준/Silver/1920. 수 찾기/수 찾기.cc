0d4ef1cd37bde4993a609882c5a494f613f07dc9
coouir <166575866+coouir@users.noreply.github.com>
2025-01-13 14:32:34 +0900

    [Silver IV] Title: 수 찾기, Time: 68 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, arrN[100000], M;
+
+bool binary_search(int target) {
+    int low = 0, high = N-1;
+    while (low <= high) {
+        int mid = (low+high)/2;
+        if (target == arrN[mid]) return true;
+        if (target < arrN[mid]) {
+            high = mid-1;
+        } else if (target > arrN[mid]) {
+            low = mid+1;
+        }
+    }
+    return false;
+}
+
+int main() {
+    scanf("%d", &N);
+    for (int i=0; i<N; i++) {
+        scanf("%d", &arrN[i]);
+    }
+    sort(arrN, arrN+N);
+    scanf("%d", &M);
+    for (int i=0; i<M; i++) {
+        int n;
+        scanf("%d", &n);
+        printf("%d\n", binary_search(n));
+    }
+    
+    return 0;
+}

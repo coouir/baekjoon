a538e1051db371b8da93a6f240793fdf2378547a
coouir <166575866+coouir@users.noreply.github.com>
2025-02-14 21:51:18 +0900

    [Gold V] Title: 두 용액, Time: 60 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int arr[100000];
+
+    int N;
+    cin >> N;
+    for (int i=0; i<N; i++) 
+        cin >> arr[i];
+    sort(arr, arr+N);
+
+    int min_e = arr[N-1];
+    int min_s = arr[0];
+    long long min_val = abs(min_e + min_s);
+
+    int s = 0;
+    int e = N-1;
+    while (s < e) {
+        // 정답
+        if (min_val > abs(arr[e] + arr[s])) {
+            min_val = abs(arr[e] + arr[s]);
+            min_e = arr[e];
+            min_s = arr[s];
+        }
+
+        // 이동
+        if (arr[e] + arr[s] >= 0) {
+            e--;
+        } else {
+            s++;
+        }
+    }
+    cout << min_s << ' ' << min_e;
+    
+    return 0;
+}
+
+// -99 -2 - 1 4 98
+// s
+//              e

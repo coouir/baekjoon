cdd2446715542788cda9b743a1431d0783c05ff3
coouir <166575866+coouir@users.noreply.github.com>
2025-02-19 21:01:25 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[1100] = {1, 1};
+    
+    for (int i=2; i<=1000; i++) {
+        arr[i] = (arr[i-1] + arr[i-2] + arr[i-2])%10007;
+    }
+
+    int n;
+    cin >> n;
+    cout << arr[n];
+
+    return 0;
+}
+
+// DP  0   1   2   3   4   5   6   7   8   9   ...
+//     1   1   2   3   5   8   13  21  34  55  ...

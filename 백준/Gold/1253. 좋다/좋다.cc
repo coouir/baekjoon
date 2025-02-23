b1721e7bc977fb9edef747eb3aee7d545bbc55d4
coouir <166575866+coouir@users.noreply.github.com>
2025-02-23 11:50:30 +0900

    [Gold IV] Title: 좋다, Time: 16 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -7,30 +7,31 @@ int arr[2000];
 int main() {
     int N;
     cin >> N;
-    for (int i=0; i<N; i++)
+    for (int i = 0; i < N; i++)
         cin >> arr[i];
 
-    sort(arr, arr+N);
+    sort(arr, arr + N); // 정렬
 
     int cnt = 0;
-    for (int i=0; i<N; i++) {
-        int s = 0;
-        int e = N-1;
+    
+    for (int i = 0; i < N; i++) {
+        int s = 0; 
+        int e = N - 1;
         if (s == i) s++;
         if (e == i) e--;
-
+        
         while (s < e) {
-            // 정답
+        	// 정답
             if (arr[s] + arr[e] == arr[i]) {
                 cnt++;
             }
 
             // 이동
             if (arr[s] + arr[e] > arr[i]) {
-                e--;
+                e--; // 합이 크면 줄여야 함
                 if (e == i) e--;
             } else if (arr[s] + arr[e] < arr[i]) {
-                s++;
+                s++; // 합이 작으면 늘려야 함
                 if (s == i) s++;
             } else {
                 break;
@@ -38,18 +39,6 @@ int main() {
         }
     }
     cout << cnt;
-
+    
     return 0;
 }
-
-
-
-// 1   2   3   4   5   6   7   8   9   10
-//                             i
-// s
-//                                     e
-
-// -1  0   1   2   3
-//                 i
-//         s
-//             e

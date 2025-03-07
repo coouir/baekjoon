012eed6d9d3070035626c13df3ff53ac5f4abaf0
coouir <166575866+coouir@users.noreply.github.com>
2025-03-07 14:47:11 +0900

    [Bronze V] Title: X보다 작은 수, Time: 252 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, X;
+    cin >> N >> X;
+    for (int i=0; i<N; i++) {
+        int n;
+        cin >> n;
+
+        if (n < X) {
+            cout << n << ' ';
+        }
+    }
+    return 0;
+}

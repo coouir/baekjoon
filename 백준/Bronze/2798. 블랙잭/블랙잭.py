87f351f7209380d128b712661df8a8e318f91ce3
coouir <166575866+coouir@users.noreply.github.com>
2025-07-10 13:44:55 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,11 +1,14 @@
-from itertools import combinations as cb
-
 N, M = map(int, input().split())
-l = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-r = []
-for a, b, c in cb(l, 3):
-    if a+b+c <= M:
-        r.append(a+b+c)
+answer = 0
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            s = arr[i] + arr[j] + arr[k]
+            if s > M:
+                continue
+            
+            answer = max(answer, s)
 
-print(max(r))
+print(answer)

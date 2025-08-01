0eda9c492270bff17566a913fa2d3c30df4f08d9
coouir <166575866+coouir@users.noreply.github.com>
2025-08-01 14:13:47 +0900

    [Silver III] Title: N과 M (5), Time: 168 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,7 +1,24 @@
-from itertools import permutations as pm
+def recur(depth):
+
+    if depth == M:
+        print(*ans)
+        return
+    
+    for i in range(N):
+        if visited[i]:
+            continue
+
+        visited[i] = True
+        ans[depth] = arr[i]
+        recur(depth + 1)
+        visited[i] = False
+
 
 N, M = map(int, input().split())
 arr = list(map(int, input().split()))
+arr.sort()
 
-for t in pm(sorted(arr), M):
-    print(' '.join(map(str, t)))
+visited = [False for i in range(N)]
+ans = [0 for i in range(M)]
+
+recur(0)

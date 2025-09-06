d65524b2c1909fd066bb29011c4fcaf4e75498e0
coouir <166575866+coouir@users.noreply.github.com>
2025-09-06 10:17:38 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.cc
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.cc
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
+#include <bits/stdc++.h>
 using namespace std;
 
-int arr1[26];
-int arr2[26];
+string a, b;
+int arr1[26], arr2[26], cnt;
 
 int main() {
-    string str1, str2;
-    cin >> str1 >> str2;
-    for (int i=0; i<(int)str1.size(); i++) {
-        arr1[str1[i]-97]++;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> a;
+    cin >> b;
+    for (int i=0; i<a.length(); i++) {
+        arr1[a[i] - 'a']++;
     }
-    for (int i=0; i<(int)str2.size(); i++) {
-        arr2[str2[i]-97]++;
+    for (int i=0; i<b.length(); i++) {
+        arr2[b[i] - 'a']++;
     }
 
-    int cnt = 0;
     for (int i=0; i<26; i++) {
         cnt += abs(arr1[i] - arr2[i]);
     }
     cout << cnt;
-    
+
     return 0;
 }

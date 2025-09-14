bb5cbca3abc2a3d46f3436fca97bb9bde9b8d188
coouir <166575866+coouir@users.noreply.github.com>
2025-09-14 14:29:16 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int isused[10];
+
+void func(int cur) {
+    if (cur == m) {
+        for (int i=0; i<m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i=1; i<=n; i++) {
+        if (!isused[i]) {
+            if (cur != 0 && arr[cur-1] > i) continue;
+            arr[cur] = i;
+            isused[i] = 1;
+            func(cur+1);
+            isused[i] = 0; 
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    func(0);
+
+    return 0;
+}

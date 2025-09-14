2983a6cf22ae9eb8f317bce069e8857518188363
coouir <166575866+coouir@users.noreply.github.com>
2025-09-14 15:40:26 +0900

    [Silver II] Title: N과 M (11), Time: 116 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int result[10];
+
+void func(int cur) {
+    if (cur == m) {
+        for (int i=0; i<m; i++) 
+            cout << result[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int l = 0;
+    for (int i=0; i<n; i++) {
+        if (l == arr[i]) continue;
+        result[cur] = l = arr[i];
+        func(cur + 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i=0; i<n; i++) 
+        cin >> arr[i];
+    sort(arr, arr+n);
+
+    func(0);
+
+    return 0;
+}

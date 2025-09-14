f338a9133d2c8a7e07ae81b60bc1646a99ee4b9a
coouir <166575866+coouir@users.noreply.github.com>
2025-09-14 12:56:54 +0900

    [Gold IV] Title: N-Queen, Time: 1556 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -13,20 +13,16 @@ void func(int cur) {
         return;
     }
 
-    for (int i=cur; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            if (!isused1[j] && !isused2[i+j] && !isused3[i-j+n-1]) {
-                isused1[j] = 1;
-                isused2[i+j] = 1;
-                isused3[i-j+n-1] = 1;
-                func(cur+1);
-                isused1[j] = 0;
-                isused2[i+j] = 0;
-                isused3[i-j+n-1] = 0;
-            } 
-
-            if (j == n-1) return;
-        }
+    for (int i=0; i<n; i++) {
+        if (!isused1[i] && !isused2[cur+i] && !isused3[cur-i+n-1]) {
+            isused1[i] = 1;
+            isused2[cur+i] = 1;
+            isused3[cur-i+n-1] = 1;
+            func(cur+1);
+            isused1[i] = 0;
+            isused2[cur+i] = 0;
+            isused3[cur-i+n-1] = 0;
+        } 
     }
 }
 

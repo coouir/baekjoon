7bddab45aa0d6f09ad9487adb1350d7a45c03e1e
coouir <166575866+coouir@users.noreply.github.com>
2025-09-20 12:11:43 +0900

    [Silver IV] Title: 카드, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int n, x;
+ll a[100010];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i=0; i<n; i++) 
+        cin >> a[i];
+
+    sort(a, a+n);
+
+    int cnt = 0;
+    ll mxval = -(1ll << 62) - 1;
+    int mxcnt = 0;
+
+    for (int i=0; i<n; i++) {
+        if (i == 0 || a[i-1] == a[i]) cnt++;
+        else {
+            if (cnt > mxcnt) {
+                mxcnt = cnt;
+                mxval = a[i-1];
+            }
+            cnt = 1;
+        }
+    }
+
+    if (cnt > mxcnt) mxval = a[n-1];
+    cout << mxval;
+
+    return 0;
+}

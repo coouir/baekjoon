2058d8db4ecbc9a306cc7f12ff8c14c8d548c57a
coouir <166575866+coouir@users.noreply.github.com>
2025-09-24 15:41:11 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+long long d[100];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    d[0] = 0;
+    d[1] = 1;
+
+    cin >> n;
+    for (int i=2; i<=n; i++) {
+        d[i] = d[i-1] + d[i-2];
+    }
+    cout << d[n];
+
+    return 0;
+}

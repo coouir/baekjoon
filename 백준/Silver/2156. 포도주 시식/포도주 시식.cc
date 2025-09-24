7fba7d5f2277724ee1c21c29ede40e084c8ac502
coouir <166575866+coouir@users.noreply.github.com>
2025-09-24 17:54:29 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -20,9 +20,6 @@ int main() {
 
     
     for (int i=3; i<=n; i++) {
-        // for (int j=1; j<=i-2; j++) {
-        //     d[i][1] = max(d[i][1], d[j][2] + a[i]);
-        // }
         d[i][0] = max(d[i-2][0], max(d[i-2][1], d[i-2][2]));
         d[i][0] = max(d[i-1][0], max(d[i-1][1], d[i-1][2]));
         d[i][1] = max(d[i-2][0], max(d[i-2][1], d[i-2][2])) + a[i];
@@ -30,13 +27,9 @@ int main() {
     }
 
     int M = 0;
-    for (int i=1; i<=n; i++) {
-        for (int j=0; j<3; j++) {
-            // cout << d[i][j] << ' ';
+    for (int i=1; i<=n; i++) 
+        for (int j=0; j<3; j++) 
             M = max(M, d[i][j]);
-        }
-        // cout << '\n';
-    }
     cout << M;
     
     return 0;

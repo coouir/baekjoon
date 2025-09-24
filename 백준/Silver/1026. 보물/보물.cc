1f23cff2455100b8978949dfdaf02820864a6808
coouir <166575866+coouir@users.noreply.github.com>
2025-09-24 22:37:21 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cc
+++ 백준/Silver/1026. 보물/보물.cc
@@ -1,24 +1,25 @@
-#include <iostream>
-#include <algorithm>
-#include <functional>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int N, arrA[50], arrB[50];
-    cin >> N;
-    for (int i=0; i<N; i++) {
-        cin >> arrA[i];
-    }
-    for (int i=0; i<N; i++) {
-        cin >> arrB[i];
-    }
-    sort(arrA, arrA+N);
-    sort(arrB, arrB+N, greater<int>());
+int n;
+int a[55], b[55];
 
-    int ans = 0;
-    for (int i=0; i<N; i++) {
-        ans += arrA[i]*arrB[i];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i=0; i<n; i++) cin >> a[i];
+    for (int i=0; i<n; i++) cin >> b[i];
+
+    sort(a, a+n);
+    sort(b, b+n);
+
+    int cnt = 0;
+    for (int i=0; i<n; i++) {
+        cnt += a[i] * b[n-1-i];
     }
-    cout << ans;
+    cout << cnt;
+    
     return 0;
 }

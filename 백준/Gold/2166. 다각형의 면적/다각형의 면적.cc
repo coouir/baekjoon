7b354529da2b5152e7a8b2c5f8daa1fa54385cb6
coouir <166575866+coouir@users.noreply.github.com>
2025-09-30 20:51:33 +0900

    [Gold V] Title: 다각형의 면적, Time: 4 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int n;
+double x[10010];
+double y[10010];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i=1; i<=n; i++) 
+        cin >> x[i] >> y[i];
+
+    x[n+1] = x[1];
+    y[n+1] = y[1];
+
+    long double cnt1 = 0.0l;
+    long double cnt2 = 0.0l;
+    for (int i=1; i<=n; i++) {
+        cnt1 += x[i]*y[i+1];
+        cnt2 += y[i]*x[i+1];
+    }
+
+    cout << fixed;
+    cout.precision(1);
+    cout << fabs(cnt1-cnt2)/2;
+}

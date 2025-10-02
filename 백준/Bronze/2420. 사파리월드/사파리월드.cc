497f58d5e5fcb66e035ad996978f8389aa5491a4
coouir <166575866+coouir@users.noreply.github.com>
2025-10-02 17:34:09 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+ll n, m;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    cout << abs(n-m);
+
+    return 0;
+}

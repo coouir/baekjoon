31c160cf271b6212a9dbdbc82d30a83161f116e5
coouir <166575866+coouir@users.noreply.github.com>
2025-10-07 17:47:30 +0900

    [Silver V] Title: 배열 합치기, Time: 412 ms, Memory: 22248 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,21 +1,53 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int arr[2000000];
+int n, m;
+int a[1000010];
+int b[1000010];
 
 int main() {
-    int N, M;
-    cin >> N >> M;
-    for (int i=0; i<N; i++) 
-        cin >> arr[i];
-    for (int i=N; i<N+M; i++) 
-        cin >> arr[i];
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    sort(arr, arr+N+M);
+    cin >> n >> m;
+    for (int i=0; i<n; i++)
+        cin >> a[i];
 
-    for (int i=0; i<N+M; i++) 
-        cout << arr[i] << ' ';
+    for (int i=0; i<m; i++)
+        cin >> b[i];
+
+    vector<int> v;
+
+    int idxA = 0;
+    int idxB = 0;
+    while (!(idxA == n && idxB == m)) {
+        if (idxA == n) {
+            while (idxB != m) {
+                v.push_back(b[idxB]);
+                idxB++;
+            }
+            break;
+        }
+        if (idxB == m) {
+            while (idxA != n) {
+                v.push_back(a[idxA]);
+                idxA++;
+            }
+            break;
+        }
+
+        if (a[idxA] <= b[idxB]) {
+            v.push_back(a[idxA]);
+            idxA++;
+        } else {
+            v.push_back(b[idxB]);
+            idxB++;
+        }
+    }
     
+    for (int i=0; i<n+m; i++) {
+        cout << v[i] << ' ';
+    }
+
     return 0;
 }

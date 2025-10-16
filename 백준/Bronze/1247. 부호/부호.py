ccaa204965a174ca4d2f54e588980c5940de4216
coouir <166575866+coouir@users.noreply.github.com>
2025-10-16 11:10:35 +0900

    [Bronze III] Title: 부호, Time: 7376 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.py
@@ -0,0 +1,13 @@
+for _ in range(3):
+    n = int(input())
+
+    sum = 0
+    for i in range(n):
+        sum += int(input())
+    
+    if (sum > 0):
+        print('+')
+    elif (sum < 0):
+        print('-')
+    else:
+        print('0')

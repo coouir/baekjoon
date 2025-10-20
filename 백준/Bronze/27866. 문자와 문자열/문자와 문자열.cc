778909a65be6ad013a8a8fbb05e79cba6dfa5174
coouir <166575866+coouir@users.noreply.github.com>
2025-10-20 18:45:22 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+int i;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> str >> i;
+    cout << str[i-1];
+
+    return 0;
+}
